50ac447294d62edec83d9adf888e938f6b89b550
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-12-08 20:56:22 +1100

    Render basic auth forms

--- src/components/authentication/Login/Login.js
+++ src/components/authentication/Login/Login.js
@@ -1,7 +1,61 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 const Login = () => {
+  const [ username, setUsername ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ errors, setErrors ] = useState('');
+
+  const _handleChange = (event) => {
+    const { name, value } = event.target;
+    if (name === 'username') setUsername(value);
+    if (name === 'email') setEmail(value);
+    if (name === 'password') setPassword(value);
+  };
+
+  const _handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <main>
-      <h1>Login Page</h1>
+      <h1>Log in</h1>
+
+      <form onSubmit={ _handleSubmit }>
+        <input
+          placeholder="username"
+          type="text"
+          name="username"
+          value={ username }
+          onChange={ _handleChange }
+        />
+
+        <input
+          placeholder="email"
+          type="email"
+          name="email"
+          value={ email }
+          onChange={ _handleChange }
+        />
+
+        <input
+          placeholder="password"
+          type="password"
+          name="password"
+          value={ password }
+          onChange={ _handleChange }
+        />
+
+        <button type="submit">
+          Log in
+        </button>
+
+        <div>
+          Don't have an account? <Link to='/signup'>Sign up</Link>
+        </div>
+
+      </form>
     </main>
   );
 };
--- src/components/authentication/SignUp/SignUp.js
+++ src/components/authentication/SignUp/SignUp.js
@@ -1,7 +1,71 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 const SignUp = () => {
+  const [ username, setUsername ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ passwordConfirmation, setPasswordConfirmation ] = useState('');
+  const [ errors, setErrors ] = useState('');
+
+  const _handleChange = (event) => {
+    const { name, value } = event.target;
+    if (name === 'username') setUsername(value);
+    if (name === 'email') setEmail(value);
+    if (name === 'password') setPassword(value);
+    if (name === 'passwordConfirmation') setPasswordConfirmation(value);
+  };
+
+  const _handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <main>
-      <h1>SignUp Page</h1>
+      <h1>Sign up</h1>
+
+      <form onSubmit={ _handleSubmit }>
+        <input
+          placeholder="username"
+          type="text"
+          name="username"
+          value={ username }
+          onChange={ _handleChange }
+        />
+
+        <input
+          placeholder="email"
+          type="email"
+          name="email"
+          value={ email }
+          onChange={ _handleChange }
+        />
+
+        <input
+          placeholder="password"
+          type="password"
+          name="password"
+          value={ password }
+          onChange={ _handleChange }
+        />
+
+        <input
+          placeholder="confirm password"
+          type="passwordConfirmation"
+          name="passwordConfirmation"
+          value={ passwordConfirmation }
+          onChange={ _handleChange }
+        />
+
+        <button type="submit">
+          Log in
+        </button>
+
+        <div>
+          Already have an account? <Link to='/login'>Log in</Link>
+        </div>
+
+      </form>
     </main>
   );
 };
