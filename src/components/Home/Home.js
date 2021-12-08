56bfd4fdda789f0274b40856b6cb30bdad167ad2
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-12-08 20:07:42 +1100

    Add links to login, signup routes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const Home = () => {
   return (
     <main>
       <h1>Home Page</h1>
+      <Link to='/login'>Log in</Link>
+      <Link to='/signup'>Sign Up</Link>
     </main>
   );
 };
