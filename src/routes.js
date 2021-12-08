1f10a859b26ea7a2e08f571cc6cff9bfb85e3a73
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-12-08 20:05:12 +1100

    Move Login, SignUp to /authentication

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home/Home';
-import Login from './components/Login/Login';
-import SignUp from './components/SignUp/SignUp';
+import Login from './components/authentication/Login/Login';
+import SignUp from './components/authentication/SignUp/SignUp';
 
 export const router = (
   <Routes>
